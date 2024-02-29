11c3172b6ce79e0226ee9c67cd76814ca53e6c5b
Dominik Lau <harek2388@gmail.com>
2024-02-29 18:03:05 +0100

    input preview taken from engine

--- src/components/graph_view/GraphPreview.tsx
+++ src/components/graph_view/GraphPreview.tsx
@@ -21,22 +21,22 @@ export default function GraphPreview({guid, onBodyClick}: {guid: GUID, onBodyCli
     return <div id={"pr" + guid} style={{left: pos.x, top: pos.y}} onMouseDown={onBodyClick} className="draggable previewNode">
             <div className="previewNode">
                 <div className="pipelineBar">
-                    <div>Preview</div> 
+                    <div>{node.value.name} Preview</div> 
                     {noInputs == 1 ? <></> : <InputSelection selectedInput={selectedInput} setSelectedInput={setSelectedInput} noInputs={noInputs}/>}
                 </div>
-                <InputPreview sourceId={guid} allowFullscreen={false}/>
+                {node.value.inputs.get(selectedInput - 1) ?  <InputPreview sourceId={node.value.inputs.get(selectedInput - 1)![0]} allowFullscreen={false}/> : <></>}
                 <OutputPreview sourceId={guid} allowFullscreen={false}/>
             </div>
          </div>
 }
 
 function InputSelection({selectedInput, setSelectedInput, noInputs} : {selectedInput: number, setSelectedInput: (_: number) => void, noInputs: number}){
-    const decrement = (e: React.MouseEvent) => {
+    const decrement = () => {
         if(selectedInput == 1) return;
         setSelectedInput(selectedInput - 1);
     }
 
-    const increment = (e: React.MouseEvent) => {
+    const increment = () => {
         if(selectedInput == noInputs) return;
         setSelectedInput(selectedInput + 1);
     }
