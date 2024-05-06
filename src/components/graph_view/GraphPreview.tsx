29cf8c8bab1924f7ee9b07284a7804fc309a3a5d
Roagen7 <harek2388@gmail.com>
2024-05-06 09:28:35 +0200

    mux component edge refresh fix

--- src/components/graph_view/GraphPreview.tsx
+++ src/components/graph_view/GraphPreview.tsx
@@ -1,20 +1,20 @@
-import { useContext, useState, useSyncExternalStore } from "react";
-import { GUID } from "../../engine/nodeResponse";
-import { nodeStoreContext, previewStoreContext } from "../../stores/context";
+import {useContext, useState, useSyncExternalStore} from "react";
+import {GUID} from "../../engine/nodeResponse";
+import {nodeStoreContext, previewStoreContext} from "../../stores/context";
 import PreviewContainer from "../preview_container/PreviewContainer";
-import { InputPreview, OutputPreview } from "../preview_container/Preview";
+import {InputPreview, OutputPreview} from "../preview_container/Preview";
 
 import "./GraphPreview.css";
-import { Button, FormControl } from "react-bootstrap";
-import { faArrowLeft, faArrowRight, faLeftLong, faRightLong } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {Button, FormControl} from "react-bootstrap";
+import {faArrowLeft, faArrowRight, faLeftLong, faRightLong} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import FormRange from "react-bootstrap/esm/FormRange";
-import { CircleSwitch } from "../CircleSwitch";
-import { Channel } from "../../stores/storeInterfaces";
+import {CircleSwitch} from "../CircleSwitch";
+import {Channel} from "../../stores/storeInterfaces";
 
-export default function GraphPreview({guid, onBodyClick}: {guid: GUID, onBodyClick?: (e : React.PointerEvent)=>void}){
-    
-    const nodeContext = useContext(nodeStoreContext)    
+export default function GraphPreview({guid, onBodyClick}: {guid: GUID, onBodyClick?: (e: React.PointerEvent) => void}) {
+
+    const nodeContext = useContext(nodeStoreContext)
     const node = nodeContext.getNode(guid)();
     let noInputs = node.value.meta.input_size;
     const pos = node.value.getPreviewPos();
@@ -24,61 +24,61 @@ export default function GraphPreview({guid, onBodyClick}: {guid: GUID, onBodyCli
     const previewStore = previewContext.getPreviewStore(guid)!;
     const previewSelections = useSyncExternalStore(previewStore.subscribeSelection.bind(previewStore) as any, previewStore.getSelection.bind(previewStore))
     const context = useSyncExternalStore(previewStore.subscribeContext.bind(previewStore) as any, previewStore.getContext.bind(previewStore))
-    
-    function handleChooseChannel(channel: Channel){
+
+    function handleChooseChannel(channel: Channel) {
         const selection = previewStore.getSelection()
-        if(selection.channel == channel){
+        if (selection.channel == channel) {
             channel = Channel.NONE
         }
         previewStore.updateSelection(selection.pointer, selection.preview, channel)
     }
 
     return <div id={"pr" + guid} style={{left: pos.x, top: pos.y}} onPointerDown={onBodyClick} className="draggable previewNode">
-            <div className="previewNode">
-                <div className="pipelineBar">
-                    <div>{node.value.name} Preview</div> 
-                    {noInputs == 1 && previewStore.getContext().visualizationEnabled ? <></> : <InputSelection selectedInput={selectedInput} setSelectedInput={setSelectedInput} noInputs={noInputs}/>}
-                </div>
-                
-                {previewStore.getContext().visualizationEnabled && node.value.inputs.get(selectedInput - 1) ?
-                <div><InputPreview sourceId={node.value.inputs.get(selectedInput - 1)![0]} previewName={guid} allowFullscreen={false}/></div> : <></>}
-                <OutputPreview sourceId={guid} allowFullscreen={false}/>
-                {
-                    previewStore.getContext().visualizationEnabled ? 
-                        <>
+        <div className="previewNode">
+            <div className="pipelineBar">
+                <div>{node.value.name} Preview</div>
+                {noInputs == 1 || !previewStore.getContext().visualizationEnabled ? <></> : <InputSelection selectedInput={selectedInput} setSelectedInput={setSelectedInput} noInputs={noInputs} />}
+            </div>
+
+            {previewStore.getContext().visualizationEnabled && node.value.inputs.get(selectedInput - 1) ?
+                <div><InputPreview sourceId={node.value.inputs.get(selectedInput - 1)![0]} previewName={guid} allowFullscreen={false} /></div> : <></>}
+            <OutputPreview sourceId={guid} allowFullscreen={false} />
+            {
+                previewStore.getContext().visualizationEnabled ?
+                    <>
                         <center>
                             <div className='border-0 bg-transparent'>
-                                    {
-                                        colorsChannels.map((item, i) => 
-                                            <CircleSwitch key={i} color={item} state={previewSelections.channel == item} toggleState={() => {handleChooseChannel(item)}}/>
-                                        )
-                                    }
+                                {
+                                    colorsChannels.map((item, i) =>
+                                        <CircleSwitch key={i} color={item} state={previewSelections.channel == item} toggleState={() => {handleChooseChannel(item)}} />
+                                    )
+                                }
                             </div>
                         </center>
                         {previewSelections.channel != Channel.NONE ? node.value.visualizationView(guid) : <></>}
-                        </>
-                        : <></>
-                }
-                    
-                    
-            </div>
-         </div>
+                    </>
+                    : <></>
+            }
+
+
+        </div>
+    </div>
 }
 
-function InputSelection({selectedInput, setSelectedInput, noInputs} : {selectedInput: number, setSelectedInput: (_: number) => void, noInputs: number}){
+function InputSelection({selectedInput, setSelectedInput, noInputs}: {selectedInput: number, setSelectedInput: (_: number) => void, noInputs: number}) {
     const decrement = () => {
-        if(selectedInput == 1) return;
+        if (selectedInput == 1) return;
         setSelectedInput(selectedInput - 1);
     }
 
     const increment = () => {
-        if(selectedInput == noInputs) return;
+        if (selectedInput == noInputs) return;
         setSelectedInput(selectedInput + 1);
     }
 
     const buttonStyle = {color: "black", display: "inline"};
 
-    return <> 
+    return <>
         <Button className="border-0 bg-transparent fg-black" onClick={decrement} style={buttonStyle}><FontAwesomeIcon icon={faLeftLong} /></Button>
         <span>{selectedInput}/{noInputs}</span>
         <Button className="border-0 bg-transparent" onClick={increment} style={buttonStyle}><FontAwesomeIcon icon={faRightLong} /></Button>
--- src/components/transforms/MuxComponent.tsx
+++ src/components/transforms/MuxComponent.tsx
@@ -1,15 +1,15 @@
-import { useState, useContext, useSyncExternalStore, useEffect } from 'react'
-import { faHand, faMagnifyingGlassPlus } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {useState, useContext, useSyncExternalStore, useEffect} from 'react'
+import {faHand, faMagnifyingGlassPlus} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {Button, Form, FormSelect} from 'react-bootstrap';
-import { FilterStoreContext } from '../../stores/simpleFilterStore';
-import { GUID } from '../../engine/engine';
-import { connectionStoreContext, nodeStoreContext } from '../../stores/context';
-import { previewStoreContext } from '../../stores/context';
+import {FilterStoreContext} from '../../stores/simpleFilterStore';
+import {GUID} from '../../engine/engine';
+import {connectionStoreContext, nodeStoreContext} from '../../stores/context';
+import {previewStoreContext} from '../../stores/context';
 
 const maxMuxInputs = 5;
 
-export default function MuxComponent({guid}: {guid: GUID}){    
+export default function MuxComponent({guid}: {guid: GUID}) {
     const nodeContext = useContext(nodeStoreContext);
     const node = useSyncExternalStore(nodeContext.subscribeNode(guid), nodeContext.getNode(guid));
     const connectionContext = useContext(connectionStoreContext);
@@ -22,62 +22,58 @@ export default function MuxComponent({guid}: {guid: GUID}){
 
     const handleSelectedChange = (value: string) => {
         const iVal = Number.parseInt(value)
-        // setSelected(iVal)
         nodeContext.updateParam(guid, {
-            "selected" : iVal
+            "selected": iVal
         })
     };
 
     const handleMuxedInputsChange = (value: string) => {
         const iVal = Number.parseInt(value)
-        if(iVal < 1 || iVal > maxMuxInputs) return;
-        if(iVal < muxedInputs && node.value.inputs.get(iVal)){
-            console.log("Here!")
-            console.log(node)
-            console.log(iVal)
+        if (iVal < 1 || iVal > maxMuxInputs) return;
+        if (iVal < muxedInputs && node.value.inputs.get(iVal)) {
             const [delGUID, _] = node.value.inputs.get(iVal)!
             connectionContext.disconnectNodes([
                 [delGUID, 0],
                 [guid, iVal]
             ])
         }
-        // setMuxedInputs(iVal);
         nodeContext.updateParam(guid, {
             "muxedInputs": iVal
         })
+        connectionContext.forceConnectionsRefresh()
     }
 
     return <div className="grid">
-        {/* <div>
+        <div>
             <label>
                 Muxed inputs:
-                <input                  type="number"
-                                        className="form-control"
-                                        value={muxedInputs}
-                                        onChange={(e) => handleMuxedInputsChange(e.target.value)}
-                                        onKeyDown={(e) => e.preventDefault()}
-                                        />
+                <input type="number"
+                    className="form-control"
+                    value={muxedInputs}
+                    onChange={(e) => handleMuxedInputsChange(e.target.value)}
+                    onKeyDown={(e) => e.preventDefault()}
+                />
             </label>
-        </div> */}
+        </div>
         <label>
             Selected input
             <div key={`inline-radio-${guid}`} className="mb-3">
                 {
                     [...Array(muxedInputs)].map((_, ix) => {
                         return <Form.Check
-                        inline
-                        label={`${ix + 1}`}
-                        key={`group-${guid}-${ix}`}
-                        type="radio"
-                        value={ix}
-                        checked={selected == ix}
-                        onChange={(e) => handleSelectedChange(e.target.value)}
+                            inline
+                            label={`${ix + 1}`}
+                            key={`group-${guid}-${ix}`}
+                            type="radio"
+                            value={ix}
+                            checked={selected == ix}
+                            onChange={(e) => handleSelectedChange(e.target.value)}
                         />
                     })
 
                 }
-            </div>            
+            </div>
         </label>
     </div>
-    
+
 }
