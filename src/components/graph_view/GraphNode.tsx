05d81780d6f2da0168fd54f74946049981656830
Roagen7 <harek2388@gmail.com>
2024-05-06 09:59:12 +0200

    fixed refresh bug in mux

--- src/components/graph_view/GraphNode.tsx
+++ src/components/graph_view/GraphNode.tsx
@@ -1,12 +1,13 @@
 import React from "react"
-import { ReactNode, useContext, useState, useSyncExternalStore } from "react";
-import { GUID } from "../../engine/engine";
-import { Button, Card, CardBody, CardHeader, CardTitle, Collapse } from "react-bootstrap";
-import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {ReactNode, useContext, useState, useSyncExternalStore} from "react";
+import {GUID} from "../../engine/engine";
+import {Button, Card, CardBody, CardHeader, CardTitle, Collapse} from "react-bootstrap";
+import {faChevronDown, faChevronUp} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 import './GraphNode.css';
-import { connectionStoreContext, nodeStoreContext, persistenceContext } from "../../stores/context";
+import {connectionStoreContext, nodeStoreContext, persistenceContext} from "../../stores/context";
+import Transform from "../../engine/Transform";
 
 interface NodeBodyProps {
     children: ReactNode;
@@ -20,67 +21,68 @@ interface NodeProps {
     children: ReactNode;
     guid: GUID;
     style?: React.CSSProperties;
-    onBodyClick?: (e : React.PointerEvent)=>void;
+    onBodyClick?: (e: React.PointerEvent) => void;
     ioFunction?: IOFunctionType
     // mouseOver&touchOver (for inputs/node itselt) - for creating edges
     // touch (handle click and drag) - for selecting and drag
     // touch (handle click and drag)
-    
+
 }
 
 
-const GraphNode: React.FC<NodeProps> = ({ children, 
+const GraphNode: React.FC<NodeProps> = ({children,
     guid,
     style,
     onBodyClick,
     ioFunction
-    }) => {
-    const nodeContext = useContext(nodeStoreContext) 
+}) => {
+    const nodeContext = useContext(nodeStoreContext)
     const connectionContext = useContext(connectionStoreContext);
 
     const node = useSyncExternalStore(nodeContext.subscribeNode(guid), nodeContext.getNode(guid));
+    //const inputSize = useSyncExternalStore(nodeContext.subscribeNode(guid), node.value.getInputSize.bind(node.val))
     const [open, setOpen] = useState(node.value.getExpanded());
-    
+
     const handleOpenClick = () => {
         node.value.setExpanded(!open)
         setOpen(!open)
         nodeContext.updateParam(guid, 0)
     }
-    
+
     const inputs = <div className="circle-container">
         {
             [...Array(node.value.meta.input_size)].map(
-                (_,i) => <div key={`input-${guid}-${i}`}  className="circle circle-top" onMouseDown={(e) => ioFunction ? ioFunction(e, guid, i) : {}}></div>
+                (_, i) => <div key={`input-${guid}-${i}`} className="circle circle-top" onMouseDown={(e) => ioFunction ? ioFunction(e, guid, i) : {}}></div>
             )
         }
     </div>
     const outputs = <div className="circle-container"><div className="circle circle-bottom" onMouseDown={(e) => ioFunction ? ioFunction(e, guid, 0) : {}}></div></div>
-    return  <div className="draggable transformNode" id={guid} key={guid} style={{left: node.value.getPos().x, top: node.value.getPos().y}}>
-            {inputs}
-            <div className="graphNode" onPointerDown={onBodyClick}>   
-                <Card className="transformCard" style={style}>
-                    <Card.Header className="cardHeader">
-                        {`${node.value.getName()} : ${node.value.meta.id}`}
-                        <div>
-                            <Button 
-                                className='border-0 bg-transparent'
-                                onClick={handleOpenClick}
-                                aria-expanded={open}>
-                                <FontAwesomeIcon className="iconInCard" icon={open ? faChevronDown : faChevronUp} />
-                            </Button>
-                        </div>
-                    </Card.Header>
-                    <Collapse in={open}  timeout={0}
+    return <div className="draggable transformNode" id={guid} key={guid} style={{left: node.value.getPos().x, top: node.value.getPos().y}}>
+        {inputs}
+        <div className="graphNode" onPointerDown={onBodyClick}>
+            <Card className="transformCard" style={style}>
+                <Card.Header className="cardHeader">
+                    {`${node.value.getName()} : ${node.value.meta.id}`}
+                    <div>
+                        <Button
+                            className='border-0 bg-transparent'
+                            onClick={handleOpenClick}
+                            aria-expanded={open}>
+                            <FontAwesomeIcon className="iconInCard" icon={open ? faChevronDown : faChevronUp} />
+                        </Button>
+                    </div>
+                </Card.Header>
+                <Collapse in={open} timeout={0}
                     onExited={() => connectionContext.forceConnectionsRefresh()}
                     onEntered={() => connectionContext.forceConnectionsRefresh()}>
-                        <Card.Body>
-                                {children ?? node.value.paramView(guid)}
-                        </Card.Body> 
-                    </Collapse>
-                </Card>
-            </div>
-            {outputs}
+                    <Card.Body>
+                        {children ?? node.value.paramView(guid)}
+                    </Card.Body>
+                </Collapse>
+            </Card>
         </div>
+        {outputs}
+    </div>
 };
 
 export default GraphNode;
--- src/engine/engine.ts
+++ src/engine/engine.ts
@@ -1,9 +1,9 @@
-import { jsonArrayMember, jsonMapMember, jsonMember, jsonObject } from "typedjson";
-import Transform, { KVParams } from "./Transform";
+import {jsonArrayMember, jsonMapMember, jsonMember, jsonObject} from "typedjson";
+import Transform, {KVParams} from "./Transform";
 import mapToTransform, {knownTypes} from "./TransformDeclarations";
-import { connect, disconnect } from './node';
-import { NodeResponse } from './nodeResponse';
-import { IEngine } from "./iengine";
+import {connect, disconnect} from './node';
+import {NodeResponse} from './nodeResponse';
+import {IEngine} from "./iengine";
 
 export type GUID = string;
 // TODO: add mechanism to detect locked state (count if any process is executing)
@@ -27,30 +27,30 @@ export class Engine extends EventTarget implements IEngine<Transform>{
     internal: EventTarget // i'm not sure if this is sequence stable for now batches
     // engine should call node in async
     // this is only for async return (ensure that some msg is always send, otherwise batching in this implementation will break) 
-    
+
     inTransaction: boolean = false;
 
     batchState: {
-       response: ExternalEngineResponse,
-       startedUpdates: Set<GUID>,
-       pendingUpdates: Set<GUID>, // basicly open updates, that wait for return
-       doneUpdates: Set<GUID>,
-       tick: number,
-       concurent_updates: number;
+        response: ExternalEngineResponse,
+        startedUpdates: Set<GUID>,
+        pendingUpdates: Set<GUID>, // basicly open updates, that wait for return
+        doneUpdates: Set<GUID>,
+        tick: number,
+        concurent_updates: number;
     }
 
 
     @jsonMapMember(String, Transform)
-    nodes: Map<GUID,Transform>
+    nodes: Map<GUID, Transform>
 
     @jsonArrayMember(String)
     source_nodes: GUID[]
 
-    constructor(){
+    constructor() {
         super()
         this.internal = new EventTarget();
         this.batchState = {
-            tick:1,
+            tick: 1,
             response: new ExternalEngineResponse(),
             startedUpdates: new Set(),
             pendingUpdates: new Set(),
@@ -59,40 +59,40 @@ export class Engine extends EventTarget implements IEngine<Transform>{
         }
         this.nodes = new Map()
         this.source_nodes = []
-        this.internal.addEventListener("info",this.handleInternalInfo.bind(this) as any)
-        this.internal.addEventListener("connection_remove",this.handleInternalConnectionsRemove.bind(this) as any)
+        this.internal.addEventListener("info", this.handleInternalInfo.bind(this) as any)
+        this.internal.addEventListener("connection_remove", this.handleInternalConnectionsRemove.bind(this) as any)
     }
 
-    public requestUpdate(node:GUID): void{
+    public requestUpdate(node: GUID): void {
         this.batchState.pendingUpdates.add(node);
     }
-    protected transactionStart(): void{
+    protected transactionStart(): void {
         this.inTransaction = true;
     }
 
-    protected transactionCommit(): void{
+    protected transactionCommit(): void {
         this.inTransaction = false;
         this.startUpdate();
     }
 
-    public _dispatch_update(id: GUID){
-        if(!this.batchState.startedUpdates.has(id)){
+    public _dispatch_update(id: GUID) {
+        if (!this.batchState.startedUpdates.has(id)) {
             this.batchState.pendingUpdates.add(id);
             const node = this.getNode(id)
-            if(node){
-                if(node.dispatch_update(this.batchState.tick)){
+            if (node) {
+                if (node.dispatch_update(this.batchState.tick)) {
                     this.batchState.startedUpdates.add(id);
                     this.batchState.concurent_updates += 1;
                 }
-            }else{
+            } else {
                 this.batchState.pendingUpdates.delete(id);
                 console.error(`Node: ${id} not exist`)
             }
-            
+
         }
     }
-    public _direct_update(id: GUID){
-        if(!this.batchState.startedUpdates.has(id)){
+    public _direct_update(id: GUID) {
+        if (!this.batchState.startedUpdates.has(id)) {
             this.batchState.startedUpdates.add(id);
             this.batchState.pendingUpdates.add(id);
             this.batchState.concurent_updates += 1;
@@ -100,40 +100,40 @@ export class Engine extends EventTarget implements IEngine<Transform>{
         }
     }
 
-    public async startUpdate(): Promise<void>{
+    public async startUpdate(): Promise<void> {
         if (this.inTransaction) return;
-        this.source_nodes.forEach((id) =>{
+        this.source_nodes.forEach((id) => {
             this._direct_update(id);
         })
-        if (this.batchState.concurent_updates == 0){
+        if (this.batchState.concurent_updates == 0) {
             console.log("dispatching pending (no source)")
             this.deadLockForceUpdate();
         }
-        if(this.batchState.concurent_updates == 0){
+        if (this.batchState.concurent_updates == 0) {
             console.log("Empty update");
             this.flushUpdate();
         }
     }
 
-    public async startUpdateAll(): Promise<void>{
-        this.nodes.forEach((node) =>{
+    public async startUpdateAll(): Promise<void> {
+        this.nodes.forEach((node) => {
             if (node.inputs.size != 0) return;
             this._direct_update(node.meta.id);
         })
-        if (this.batchState.concurent_updates == 0){
+        if (this.batchState.concurent_updates == 0) {
             console.log("dispatching pending (no source)")
             this.deadLockForceUpdate();
         }
-        if(this.batchState.concurent_updates == 0){
+        if (this.batchState.concurent_updates == 0) {
             console.log("Empty update");
             this.flushUpdate();
         }
     }
 
-    private flushUpdate():void{
+    private flushUpdate(): void {
         // save deleted nodes state 
         this.batchState.response.node.removed_nodes = [...this.batchState.response.node.removed.map(v => this.getNode(v)!)]
-        this.dispatchEvent(new CustomEvent<ExternalEngineResponse>("update",{detail:this.batchState.response}))
+        this.dispatchEvent(new CustomEvent<ExternalEngineResponse>("update", {detail: this.batchState.response}))
         // clean detached nodes
         this.batchState.response.node.removed.forEach(v => this.nodes.delete(v));
         const tick = this.batchState.tick + 1;
@@ -147,73 +147,74 @@ export class Engine extends EventTarget implements IEngine<Transform>{
         }
     }
 
-    private deadLockForceUpdate(): void{
-        this.batchState.pendingUpdates.forEach(v =>{
+    private deadLockForceUpdate(): void {
+        this.batchState.pendingUpdates.forEach(v => {
             this._direct_update(v);
         })
     }
 
-    public updateNodeParams(node:GUID,params:KVParams): void{
+    public updateNodeParams(node: GUID, params: KVParams): void {
         const transform = this.nodes.get(node)
-        if (transform === undefined){
+        if (transform === undefined) {
             // TODO think about some error handling
             return
         }
-        for (const key in params){
-            this.batchState.response.node.updated_params.push({node_id:node,key,old: transform.params[key],new: params[key]})
+        for (const key in params) {
+            this.batchState.response.node.updated_params.push({node_id: node, key, old: transform.params[key], new: params[key]})
         }
+        console.log("here!!")
         // if found add to pending
         transform.updateParams(params);
         transform.hash = crypto.randomUUID();
         this.requestUpdate(transform.meta.id);
         this.startUpdate();
     }
-    public _addNode(node: Transform){
+    public _addNode(node: Transform) {
         const guid = node.meta.id;
-        if (node.meta.input_size == 0){
+        if (node.meta.input_size == 0) {
             this.source_nodes.push(guid)
         }
         node.engine = this;
-        this.nodes.set(guid,node)
+        this.nodes.set(guid, node)
         this.batchState.response.node.added.push(guid);
         this.requestUpdate(guid);
-        this.startUpdate(); 
+        this.startUpdate();
     }
 
-    public addNode(transformation: string, params: any): GUID{
+    public addNode(transformation: string, params: any): GUID {
         const node = mapToTransform(transformation)!
-        if(params.position){
+        if (params.position) {
             node.setPos(params.position);
         }
-        if(params.previewPosition){
+        if (params.previewPosition) {
             node.setPreviewPos(params.previewPosition);
         }
         this._addNode(node);
         return node.meta.id;
     }
 
-    public removeNode(guid:GUID){
+    public removeNode(guid: GUID) {
         let node = this.getNode(guid);
         node?.disconnect(); // this will not trigger update, only request them
         // this.nodes.delete(guid);
         this.batchState.response.node.removed.push(guid);
-        this.source_nodes = this.source_nodes.filter((v) => v!= guid);
+        this.source_nodes = this.source_nodes.filter((v) => v != guid);
         this.startUpdate();
     }
 
-    public connectNodes(source:GUID,destination:GUID,source_handle: number,destination_handle:number): boolean{
-        let ok = connect(this.nodes.get(source)!,source_handle,this.nodes.get(destination)!,destination_handle)
-        if (ok){
-            this.batchState.response.connection.added.push([[source,source_handle],[destination,destination_handle]]);
+    public connectNodes(source: GUID, destination: GUID, source_handle: number, destination_handle: number): boolean {
+        let ok = connect(this.nodes.get(source)!, source_handle, this.nodes.get(destination)!, destination_handle)
+        if (ok) {
+            this.batchState.response.connection.added.push([[source, source_handle], [destination, destination_handle]]);
             this.startUpdate();
         }
         return ok;
     }
 
-    public disconnectNodes(source:GUID,destination:GUID,source_handle: number,destination_handle:number): boolean{
-        let ok = disconnect(this.nodes.get(source)!,source_handle,this.nodes.get(destination)!,destination_handle)
-        if (ok){
-            this.batchState.response.connection.removed.push([[source,source_handle],[destination,destination_handle]]);
+    public disconnectNodes(source: GUID, destination: GUID, source_handle: number, destination_handle: number): boolean {
+        let ok = disconnect(this.nodes.get(source)!, source_handle, this.nodes.get(destination)!, destination_handle)
+        if (ok) {
+            this.batchState.response.connection.removed.push([[source, source_handle], [destination, destination_handle]]);
             this.startUpdate();
         } else {
             console.log("didn't delete it")
@@ -221,57 +222,57 @@ export class Engine extends EventTarget implements IEngine<Transform>{
         return ok;
     }
 
-    public getNode(node:GUID): Transform | undefined {
+    public getNode(node: GUID): Transform | undefined {
         return this.nodes.get(node)
     }
 
-    public update_all(){
+    public update_all() {
         this.flushUpdate();
         this.startUpdateAll();
     }
 
     private handleInternalInfo(event: CustomEvent<NodeResponse>) {
-        
-        if (event.detail.status == "updated"){
+
+        if (event.detail.status == "updated") {
             const msg = event.detail;
-            msg.requestUpdates.forEach((id) =>{
+            msg.requestUpdates.forEach((id) => {
                 this._dispatch_update(id);
             })
             this.batchState.response.node.updated.push(msg.nodeId);
-        }else if (event.detail.status == "error"){
+        } else if (event.detail.status == "error") {
             const msg = event.detail;
-            msg.invalidateChildrens.forEach((id) =>{
+            msg.invalidateChildrens.forEach((id) => {
                 this._dispatch_update(id);
             })
             this.batchState.response.node.errors.push(event.detail.nodeId);
 
-        }else{
+        } else {
             console.log("undefined msg type");
         }
-        if (!this.batchState.doneUpdates.has(event.detail.nodeId)){
+        if (!this.batchState.doneUpdates.has(event.detail.nodeId)) {
             this.batchState.doneUpdates.add(event.detail.nodeId);
-            this.batchState.concurent_updates-=1;
+            this.batchState.concurent_updates -= 1;
         }
         // TODO: temporary
         this.getNode(event.detail.nodeId)!.hash = crypto.randomUUID();
 
-        if(this.batchState.concurent_updates == 0){
+        if (this.batchState.concurent_updates == 0) {
 
-            
+
             // Desired state all required nodes updated
-            if (this.batchState.startedUpdates.size == this.batchState.doneUpdates.size && this.batchState.doneUpdates.size === this.batchState.pendingUpdates.size){
+            if (this.batchState.startedUpdates.size == this.batchState.doneUpdates.size && this.batchState.doneUpdates.size === this.batchState.pendingUpdates.size) {
                 this.flushUpdate();
                 console.log("batch reset"); // TODO: Remove this later
                 return;
-            }else{ // no update is running (deadlock)
+            } else { // no update is running (deadlock)
                 // TODO: detect if deadlock is caused by loop or disconnected graph part
                 console.log("batch deadlock");
                 this.deadLockForceUpdate();
             }
         }
     }
-    
-    private handleInternalConnectionsRemove(event: CustomEvent<[[GUID,number],[GUID,number]]>){
+
+    private handleInternalConnectionsRemove(event: CustomEvent<[[GUID, number], [GUID, number]]>) {
         this.batchState.response.connection.removed.push(event.detail);
         // kick node to recalculate state update in child node
         // this.batchState.pendingUpdates.add(event.detail[1][0]);
@@ -282,7 +283,7 @@ export class Engine extends EventTarget implements IEngine<Transform>{
         // this.dispatchEvent(new CustomEvent<ExternalEngineResponse>("update",{detail:this.batchState.response}))
     }
 
-    private handleInternalConnectionsAdd(event: CustomEvent<[[GUID,number],[GUID,number]]>){
+    private handleInternalConnectionsAdd(event: CustomEvent<[[GUID, number], [GUID, number]]>) {
         this.batchState.response.connection.added.push(event.detail);
         // kick node to recalculate state update in child node
         this.batchState.pendingUpdates.add(event.detail[1][0]);
@@ -291,31 +292,31 @@ export class Engine extends EventTarget implements IEngine<Transform>{
         // this.dispatchEvent(new CustomEvent<ExternalEngineResponse>("update",{detail:this.batchState.response}))
     }
 
-    public fixSerialization():void{
+    public fixSerialization(): void {
         this.nodes.forEach(n => n.engine = this);
         this.update_all();
     }
 }
 
 // this will be send to parent element to inform that some element changed
-export class ExternalEngineResponse{
-    isHistoryUpdate: boolean = false; 
-    node :{
+export class ExternalEngineResponse {
+    isHistoryUpdate: boolean = false;
+    node: {
         updated: GUID[] // all nodes that finished update successfully
         errors: GUID[]   // all nodes that returned error
         added: GUID[]
         removed: GUID[]
         removed_nodes: Transform[];
-        updated_params: {node_id: GUID,key: string,old: any,new: any}[]
+        updated_params: {node_id: GUID, key: string, old: any, new: any}[]
     }
 
-    connection : {
-        added: [[GUID,number],[GUID,number]][];
-        removed: [[GUID,number],[GUID,number]][]
+    connection: {
+        added: [[GUID, number], [GUID, number]][];
+        removed: [[GUID, number], [GUID, number]][]
     }
-    constructor(){
+    constructor() {
         this.node = {
-            added : [],
+            added: [],
             errors: [],
             removed: [],
             updated: [],
