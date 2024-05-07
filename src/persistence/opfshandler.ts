7f45b70163259607b34a9fdd17e11c35c95db736
Roagen7 <harek2388@gmail.com>
2024-05-07 11:05:58 +0200

    further experiments with opfs

--- src/persistence/opfshandler.ts
+++ src/persistence/opfshandler.ts
@@ -1,15 +1,29 @@
 import KVHandlerIfc from "./kvhandler";
 
 export default class OPFSHandler implements KVHandlerIfc {
-    async write(key: string, value: string) {
-        const root = await navigator.storage.getDirectory()
-        const file = await root.getFileHandle(key, {
-            create: true,
-        })
-        //@ts-ignore
-        const wrt = await file.createWritable()
-        await wrt.write(value)
-        await wrt.close()
+
+    fileHandle?: FileSystemFileHandle
+
+    constructor() {
+        this.constructorAsync()
+    }
+
+    async constructorAsync() {
+        const opfsRoot = await navigator.storage.getDirectory();
+        this.fileHandle = await opfsRoot.getFileHandle("storage", {create: true});
+    }
+
+    write(key: string, value: string) {
+
+        // const root = await navigator.storage.getDirectory()
+        // const file = await root.getFileHandle(key, {
+        //     create: true,
+        // })
+        // //@ts-ignore
+        // const wrt = await file.createWritable()
+
+        // await wrt.write(value)
+        // await wrt.close()
     }
     read(key: string) {
         return ""
